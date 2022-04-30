bf88f987907f5e6340a89d03f1f6b778b6a80620
fdesp <209364055@cua.uam.mx>
2022-04-30 18:27:23 -0500

    frontend: calls backend to register new users

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [AppComponent, SignupPageComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup-page',
@@ -10,8 +11,28 @@ export class SignupPageComponent implements OnInit {
   public mail: string = '';
   public password: string = '';
   public userExists: boolean = false;
-
-  constructor() {}
+  private API = 'http://127.0.0.1:3000/signup';
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  submit(): void {
+    this.userExists = false;
+    const headers = { 'Content-Type': 'application/json' };
+    this.http
+      .post(
+        this.API,
+        {
+          user: this.user,
+          mail: this.mail,
+          password: this.password,
+        },
+        { headers }
+      )
+      .subscribe((data: any) => {
+        if (data.result === 'error') {
+          this.userExists = true;
+        }
+      });
+  }
 }
