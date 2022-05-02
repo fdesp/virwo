17326a3916c5cc0370c234a47b11f3a808a60578
fdesp <209364055@cua.uam.mx>
2022-05-01 23:33:24 -0500

    frontend: enable feeds api

--- frontend/src/app/news-feed-page/news-feed-page.component.ts
+++ frontend/src/app/news-feed-page/news-feed-page.component.ts
@@ -12,96 +12,10 @@ export class NewsFeedPageComponent implements OnInit {
   public from: string = '';
   private pageSize: number = 10;
   private API = 'http://127.0.0.1:3000/feed';
-  //public articles: any;
-
-  public articles = [
-    {
-      title: 'Pair Mixes It up + Hacks Its Own Product for a Client Request',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title: 'Pair Mixes It up + Hacks Its Own Product for a Client Request',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title: 'Pair Mixes It up + Hacks Its Own Product for a Client Request',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title: 'Pair Mixes It up + Hacks Its Own Product for a Client Request',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title: 'Pair Mixes It up + Hacks Its Own Product for a Client Request',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title: 'React vs. Angular: Why Is React So Much More Popular?',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        'How do you choose between these two popular front-end frameworks? Start by looking at their performance and what they’re like to code in.',
-      url: 'https://www.makeuseof.com/react-vs-angular-popular/',
-      urlToImage:
-        'https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2022/03/HTML-Code-on-a-Computer-Screen.jpg',
-    },
-    {
-      title:
-        'heise-Angebot: enterJS 2022: Frühbucherrabatt noch bis zum 4. Mai sichern',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-    {
-      title:
-        'Architect Daniel Libeskind’s Redesigned Richard Hennessy Decanter',
-      source: {
-        name: 'Design-milk.com',
-      },
-      description:
-        "San Francisco-based Pair doesn't make lighting but it hacks its Mix collection to create custom lighting for a client's industrial office",
-      url: 'https://design-milk.com/pair-mixes-it-up-hacks-its-own-product-for-a-client-request/',
-      urlToImage: 'https://design-milk.com/images/2022/04/pair-mix-hack-11.jpg',
-    },
-  ];
+  public articles: any;
 
   constructor(private http: HttpClient) {
-    // this.requestFeed();
+    this.requestFeed();
   }
 
   ngOnInit(): void {}
@@ -126,7 +40,7 @@ export class NewsFeedPageComponent implements OnInit {
 
   updateTermSearch(query: string): void {
     this.query = query ? query : 'angular';
-    /// this.requestFeed();
+    this.requestFeed();
   }
 
   updateFilterSearch(query: string): void {
@@ -135,12 +49,11 @@ export class NewsFeedPageComponent implements OnInit {
     } else {
       this.language = '';
     }
-    console.log(this.language);
-    // this.requestFeed();
+    this.requestFeed();
   }
 
   updateDateSearch(date: string): void {
     this.from = date ? date : '';
-    // this.requestFeed();
+    this.requestFeed();
   }
 }
