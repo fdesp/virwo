cd6b07776392e3c7eb270284e8bf77b1fc5beee3
fdesp <209364055@cua.uam.mx>
2022-05-01 20:13:36 -0500

    Backend: implement newsapi.org calls

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const got = require("got");
 const db = require("./db");
 const app = express();
 const port = 3000;
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/signup", async (req, res) => {
   let user = req.body;
   try {
-    const result = await db.pool.query(
+    await db.pool.query(
       "insert into users (user, mail, password) values (?, ?, ?)",
       [user.user, user.mail, user.password]
     );
@@ -20,6 +21,29 @@ app.post("/signup", async (req, res) => {
   } catch (err) {
     res.send({ result: "error" });
   }
+});
+
+app.post("/feed", async (req, res) => {
+  let feed = req.body;
+
+  const API_KEY = "6b8ef191c38a4ae3b1573d19a4cfc12e";
+  const API = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`;
+
+  let request =
+    `${API}` +
+    (feed.query ? `&q=${feed.query}` : "") +
+    (feed.language ? `&language=${feed.language}` : "") +
+    (feed.from ? `&from=${feed.from}` : "") +
+    `&pageSize=${feed.pageSize}`;
+
+  (async () => {
+    try {
+      const response = await got(request);
+      res.send(response.body);
+    } catch (error) {
+      console.log(error.response.body);
+    }
+  })();
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
